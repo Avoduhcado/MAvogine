fb2f757fd98432290cc247fa3d65e51e8975c137
Avoduhcado <lostking9@yahoo.com>
2019-12-08 15:13:39 -0500

    Use file separator system property in ResourceConstants

--- src/main/java/com/avogine/core/resource/util/ResourceConstants.java
+++ src/main/java/com/avogine/core/resource/util/ResourceConstants.java
@@ -2,8 +2,10 @@ package com.avogine.core.resource.util;
 
 public class ResourceConstants {
 
-	public static final String MODEL_PATH = "models/";
-	public static final String SHADER_PATH = "shaders/";
-	public static final String TEXTURE_PATH = "textures/";
+	private static final String SEPARATOR = System.getProperty("file.separator");
+	
+	public static final String MODEL_PATH = "models" + SEPARATOR;
+	public static final String SHADER_PATH = "shaders" + SEPARATOR;
+	public static final String TEXTURE_PATH = "textures" + SEPARATOR;
 
 }
