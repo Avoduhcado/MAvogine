88eb3b8ae4b7b67f1fa9ab23c94e595a601a31bb
Avoduhcado <lostking9@yahoo.com>
2020-02-03 23:39:52 -0500

    Third person camera improvements
    Can rotate focus entity with right mouse button, still needs a lot of
    work.

--- base/src/main/java/com/avogine/core/scene/camera/ThirdPersonCamera.java
+++ base/src/main/java/com/avogine/core/scene/camera/ThirdPersonCamera.java
@@ -18,7 +18,6 @@ import com.avogine.core.system.listener.MouseMotionListener;
 import com.avogine.core.system.listener.MouseScrollListener;
 
 /**
- * Rotating and scrolling the mouse wheel together cause funkiness
  * @author Dominus
  *
  */
@@ -30,6 +29,10 @@ public class ThirdPersonCamera extends Camera implements MouseScrollListener, Mo
 	private boolean mouseOneDown;
 	private boolean mouseTwoDown;
 	
+	/**
+	 * @param window
+	 * @param focus
+	 */
 	public ThirdPersonCamera(Window window, Entity focus) {
 		super(window, new Vector3f(), 10, -90, 45f);
 		this.focus = focus;
@@ -43,7 +46,8 @@ public class ThirdPersonCamera extends Camera implements MouseScrollListener, Mo
 		position.set(focus.getPosition().x, focus.getPosition().y + 1.5f, focus.getPosition().z);
 		if (!mouseOneDown) {
 			// TODO Set this to lerp to the position to avoid jolts
-			setYaw((float) Math.toDegrees(focus.getOrientation().angle()) + 180);
+			float yaw = (float) -(Math.atan2(focus.getOrientation().y, focus.getOrientation().w) * 2.0f);
+			setYaw((float) Math.toDegrees(yaw) + 180);
 		}
 	}
 	
@@ -69,23 +73,37 @@ public class ThirdPersonCamera extends Camera implements MouseScrollListener, Mo
 	public void mouseClicked(MouseClickEvent event) {
 		switch (event.getType()) {
 		case GLFW.GLFW_PRESS:
-			GLFW.glfwSetInputMode(event.getWindow(), GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_DISABLED);
 			try (MemoryStack stack = MemoryStack.stackPush()) {
 				DoubleBuffer xPos = stack.mallocDouble(1);
 				DoubleBuffer yPos = stack.mallocDouble(1);
 
 				GLFW.glfwGetCursorPos(event.getWindow(), xPos, yPos);
-				setLastX((float) xPos.get());
-				setLastY((float) yPos.get());
+				lastX = (float) xPos.get();
+				lastY = (float) yPos.get();
 			}
-			if (event.getButton() == GLFW.GLFW_MOUSE_BUTTON_1) {
+			switch (event.getButton()) {
+			case GLFW.GLFW_MOUSE_BUTTON_1:
 				mouseOneDown = true;
+				break;
+			case GLFW.GLFW_MOUSE_BUTTON_2:
+				mouseTwoDown = true;
+				break;
+			}
+			if (mouseOneDown || mouseTwoDown) {
+				GLFW.glfwSetInputMode(event.getWindow(), GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_DISABLED);
 			}
 			break;
 		case GLFW.GLFW_RELEASE:
-			GLFW.glfwSetInputMode(event.getWindow(), GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_NORMAL);
-			if (event.getButton() == GLFW.GLFW_MOUSE_BUTTON_1) {
+			switch (event.getButton()) {
+			case GLFW.GLFW_MOUSE_BUTTON_1:
 				mouseOneDown = false;
+				break;
+			case GLFW.GLFW_MOUSE_BUTTON_2:
+				mouseTwoDown = false;
+				break;
+			}
+			if (!mouseOneDown && !mouseTwoDown) {
+				GLFW.glfwSetInputMode(event.getWindow(), GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_NORMAL);
 			}
 			break;
 		}
@@ -97,24 +115,34 @@ public class ThirdPersonCamera extends Camera implements MouseScrollListener, Mo
 			return;
 		}
 		
-		float xOffset = event.getXPosition() - getLastX();
-		float yOffset = getLastY() - event.getYPosition(); // reversed since y-coordinates go from bottom to top
-		setLastX(event.getXPosition());
-		setLastY(event.getYPosition());
+		float xOffset = event.getXPosition() - lastX;
+		float yOffset = lastY - event.getYPosition(); // reversed since y-coordinates go from bottom to top
+		lastX = event.getXPosition();
+		lastY = event.getYPosition();
 
 		float sensitivity = 0.1f; // TODO Make this a customizable option
 		xOffset *= sensitivity;
 		yOffset *= sensitivity;
 
-		setYaw(getYaw() + xOffset);
-		setPitch(getPitch() - yOffset);
+		yaw -= xOffset;
+		pitch -= yOffset;
 
 		// Make sure that when pitch is out of bounds, screen doesn't get flipped
-		if (getPitch() > 89.0f) {
-			setPitch(89.0f);
+		if (pitch > 89.0f) {
+			pitch = 89.0f;
 		}
-		if (getPitch() < 0f) {
-			setPitch(0f);
+		if (pitch < 0f) {
+			pitch = 0f;
+		}
+		
+//		if (!mouseOneDown) {
+//			// TODO Set this to lerp to the position to avoid jolts
+//			float yaw = (float) -(Math.atan2(focus.getOrientation().y, focus.getOrientation().w) * 2.0f);
+//			yaw = (float) Math.toDegrees(yaw) + 180;
+//		}
+		
+		if (mouseTwoDown) {
+			focus.getOrientation().rotateY((float) Math.toRadians(xOffset));
 		}
 	}
 
