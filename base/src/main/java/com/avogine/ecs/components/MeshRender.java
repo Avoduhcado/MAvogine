9720957b173d2b35e136b943f9305f28c51f472f
Avoduhcado <lostking9@yahoo.com>
2020-09-28 22:55:16 -0400

    Basic component implementation
    Added orientation/scale to Transform.
    Added simple mesh array to MeshRender.

--- base/src/main/java/com/avogine/ecs/components/MeshRender.java
+++ base/src/main/java/com/avogine/ecs/components/MeshRender.java
@@ -1,16 +1,35 @@
 package com.avogine.ecs.components;
 
 import com.avogine.ecs.EntityComponent;
+import com.avogine.render.data.Mesh;
 
 /**
  *
  */
 public class MeshRender extends EntityComponent {
 
+	private Mesh[] meshes;
+	
 	/**
 	 * 
 	 */
 	public MeshRender() {
+		meshes = new Mesh[1];
+	}
+	
+	public MeshRender(Mesh[] meshes) {
+		this.meshes = meshes;
 	}
 
+	public Mesh[] getMeshes() {
+		return meshes;
+	}
+	
+	/**
+	 * @param meshes the meshes to set
+	 */
+	public void setMeshes(Mesh[] meshes) {
+		this.meshes = meshes;
+	}
+	
 }
