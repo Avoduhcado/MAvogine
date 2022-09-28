5ac5a0914b285cc13314583039110e352d0d3314
Avoduhcado <lostking9@yahoo.com>
2022-09-28 18:13:27 -0400

    Adding additional constructors to TransformComponent
    Along with filling in some documentation.

--- base/src/main/java/com/avogine/ecs/components/TransformComponent.java
+++ base/src/main/java/com/avogine/ecs/components/TransformComponent.java
@@ -12,14 +12,37 @@ public class TransformComponent extends EntityComponent {
 	private final Vector3f position;
 	private final Quaternionf orientation;
 	private final Vector3f scale;
+
+	/**
+	 * 
+	 * @param position
+	 * @param orientation 
+	 * @param scale
+	 */
+	public TransformComponent(Vector3f position, Quaternionf orientation, Vector3f scale) {
+		this.position = position;
+		this.orientation = orientation;
+		this.scale = scale;
+	}
 	
 	/**
 	 * 
+	 * @param position
+	 * @param orientation 
 	 */
-	public TransformComponent() {
-		this(new Vector3f());
+	public TransformComponent(Vector3f position, Quaternionf orientation) {
+		this(position, orientation, new Vector3f(1.0f));
 	}
 	
+	/**
+	 * 
+	 * @param position
+	 * @param scale
+	 */
+	public TransformComponent(Vector3f position, Vector3f scale) {
+		this(position, new Quaternionf(), scale);
+	}
+
 	/**
 	 * 
 	 * @param position
@@ -30,16 +53,10 @@ public class TransformComponent extends EntityComponent {
 	
 	/**
 	 * 
-	 * @param position
-	 * @param scale
 	 */
-	public TransformComponent(Vector3f position, Vector3f scale) {
-		this.position = position;
-		orientation = new Quaternionf();
-		this.scale = scale;
+	public TransformComponent() {
+		this(new Vector3f());
 	}
-	
-	// TODO More constructors
 	
 	/**
 	 * @return the position
@@ -55,6 +72,14 @@ public class TransformComponent extends EntityComponent {
 	 */
 	public void setPosition(float x, float y, float z) {
 		this.position.set(x, y, z);
+	}
+	
+	/**
+	 * Set this transform's position to a copy of the input {@code position}.
+	 * @param position The position to set this transform to.
+	 */
+	public void setPosition(Vector3f position) {
+		this.position.set(position);
 	}
 
 	/**
@@ -74,6 +99,14 @@ public class TransformComponent extends EntityComponent {
 	public void setOrientation(float x, float y, float z, float w) {
 		this.orientation.set(x, y, z, w);
 	}
+
+	/**
+	 * Set this transform's orientation to a copy of the input {@code orientation}.
+	 * @param orientation The orientation to set this transform to.
+	 */
+	public void setOrientation(Quaternionf orientation) {
+		this.orientation.set(orientation);
+	}
 	
 	/**
 	 * @return the scale
@@ -83,10 +116,27 @@ public class TransformComponent extends EntityComponent {
 	}
 	
 	/**
-	 * 
-	 * @param scale
+	 * Set the scale to a uniform value across all 3 axes.
+	 * @param scale a value to set the x, y, z values of this transform's scale to.
 	 */
 	public void setScale(float scale) {
+		this.scale.set(scale);
+	}
+	
+	/**
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
+	public void setScale(float x, float y, float z) {
+		this.scale.set(x, y, z);
+	}
+	
+	/**
+	 * Set this transform's scale to a copy of the input {@code scale}.
+	 * @param scale The scale to set this transform to.
+	 */
+	public void setScale(Vector3f scale) {
 		this.scale.set(scale);
 	}
 
