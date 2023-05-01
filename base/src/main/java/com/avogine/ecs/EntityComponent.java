d3ba1df408efaefbe5edc769afc2888c847b29df
Avoduhcado <lostking9@yahoo.com>
2023-05-01 19:19:48 -0400

    Entity component cleanup on removal
    Added method to EntityComponent for optional cleanup.
    Added toggle for EntityChunk.removeEntity to run component cleanup.

--- base/src/main/java/com/avogine/ecs/EntityComponent.java
+++ base/src/main/java/com/avogine/ecs/EntityComponent.java
@@ -4,5 +4,13 @@ package com.avogine.ecs;
  * TODO
  */
 public interface EntityComponent {
-	
+	/**
+	 * Optional cleanup method.
+	 * </p>
+	 * This is a no-op by default but can be overriden by subclasses to perform unique
+	 * cleanup operations for components.
+	 */
+	public default void cleanup() {
+		
+	}
 }
