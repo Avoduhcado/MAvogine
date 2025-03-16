9723b4b1a2a25aab31fa2e4b1dc8c246c0ce34ad
Avoduhcado <lostking9@yahoo.com>
2025-03-16 15:14:49 -0400

    Replacing FontDetails with Font
    Improved Font implementation for easier API usage of underlying
    STBTrueType functionality.

--- base/src/main/java/com/avogine/render/FontCache.java
+++ base/src/main/java/com/avogine/render/FontCache.java
@@ -2,14 +2,14 @@ package com.avogine.render;
 
 import java.util.*;
 
-import com.avogine.render.data.FontDetails;
+import com.avogine.render.data.font.Font;
 
 /**
  *
  */
 public class FontCache {
 
-	private final Map<String, FontDetails> fontMap;
+	private final Map<String, Font> fontMap;
 	
 	/**
 	 * 
@@ -20,16 +20,18 @@ public class FontCache {
 	
 	/**
 	 * @param filePath The file path to the resource file containing the Font data to load.
-	 * @return
+	 * @param fontSizes 
+	 * @return the cached {@link Font} for the given path, or if it didn't already exist, the newly loaded {@link Font}. 
 	 */
-	public FontDetails getFont(String filePath) {
-		return fontMap.computeIfAbsent(filePath, FontDetails::new);
+	public Font getFont(String filePath, float...fontSizes) {
+		return fontMap.computeIfAbsent(filePath, k -> new Font(k, fontSizes));
 	}
 	
 	/**
 	 * 
 	 */
 	public void cleanup() {
-		fontMap.values().forEach(FontDetails::cleanup);
+		fontMap.values().forEach(Font::cleanup);
 	}
+	
 }
